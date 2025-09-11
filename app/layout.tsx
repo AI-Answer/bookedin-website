ef3a43e2ea183d34eb31cc52bdc1da99fd14e85a
Samin Yasar <saminyasar@mac.lan>
2025-09-11 18:15:18 -0400

    feat: add Meta Pixel and Google Ads gtag; keep Senja widget site-wide

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,33 @@ export default function RootLayout({
           strategy="afterInteractive"
           async
         />
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1279721066698447');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript dangerouslySetInnerHTML={{ __html: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1279721066698447&ev=PageView&noscript=1" />' }} />
+
+        {/* Google (reuse existing gtag.js loader, add Ads config) */}
+        <Script id="gtag-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11413017903');
+          `}
+        </Script>
       </body>
     </html>
   );
