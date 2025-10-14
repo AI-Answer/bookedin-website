33b438b13766060cbd50566858039bbc0411b106
Samin Yasar <saminyasar@mac.lan>
2025-10-14 11:53:18 -0400

    add Google Tag Manager (GTM-NST253KK) to head and body

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NST253KK');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <link rel="icon" href="/favicon.ico?v=1" sizes="any" />
         <link rel="shortcut icon" href="/favicon.ico?v=1" />
         <link rel="apple-touch-icon" href="/favicon.ico?v=1" />
@@ -72,6 +84,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NST253KK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <GlobalClickTracker />
         {children}
         {/* VisitorTracking tracer init (define before script loads) */}
